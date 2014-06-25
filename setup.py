f118874886ec963532bb1235dab26b29c41e3b74
Gustavo Picon <tabo@tabo.pe>
2014-06-25 03:31:15 -0500

    Preparing 2.0.1 release

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class pytest_test(test):
 
 setup_args = dict(
     name='cherrypy-dynpool',
-    version='2.0',
+    version='2.0.1',
     url='https://tabo.pe/projects/cherrypy-dynpool/',
     author='Gustavo Picon',
     author_email='tabo@tabo.pe',
